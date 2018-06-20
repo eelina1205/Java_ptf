02aa7a59077545ba4cc7ac97c78cdfbbf94bd91c
eelina1205 <eelina1205@gmail.com>
2018-06-20 15:06:53 +0200

    Исправлено 2 задание (изменила формулу вычисления прямой на плоскости)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -11,8 +11,11 @@ public class MyFirstProgram {
    Rectangle r = new Rectangle(4,6);
    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-   Point p = new Point(10,2);
-   System.out.println("Расстояние между двумя точкамии " + p.c + " и " + p.d + " = " + p.area ());
+   Point p = new Point (4, 6, 8, 10);
+   System.out.println("Расстояние между двумя точкамии " + " x1y1 " + p.x1 + " , " + p.x2 + " и " + " x2y2 " + p.y1 + " , " + p.y2 +  "= " + p.area ());
+
+
+    //distance = Math.sqrt((x1-x2)(x1-x2) + (y1-y2)(y1-y2));
 
   }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,14 +1,19 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public double c;
-  public double d;
-  public Point (double c, double d){
-    this.c = c;
-    this.d = d;
+  public double x1;
+  public double x2;
+  public double y1;
+  public double y2;
+  public Point (double x1, double x2, double y1, double y2){
+    this.x1 = x1;
+    this.x2 = x2;
+    this.y1 = y1;
+    this.y2 = y2;
 
   }
   public double area() {
-    return this.c - this.d;
+    return Math.sqrt((this.x1-this.x2)*(this.x1-this.x2) + (this.y1-this.y2)*(this.y1-this.y2));
+
   }
 }
