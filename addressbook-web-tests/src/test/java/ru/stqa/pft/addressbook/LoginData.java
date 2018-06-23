095a5ca7d5c4340637694e4550da6744274c79a0
eelina1205 <eelina1205@gmail.com>
2018-06-23 16:39:08 +0200

    Revert "Revert "Revert "Revert "Исправлено 4 задание""""
    This reverts commit b151e52057ee41be3352fe0c51ef275b7a06b1bc.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/LoginData.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook;
+
+public class LoginData {
+  private final String user;
+  private final String password;
+
+  public LoginData(String user, String password) {
+    this.user = user;
+    this.password = password;
+  }
+
+  public String getUser() {
+    return user;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+}
