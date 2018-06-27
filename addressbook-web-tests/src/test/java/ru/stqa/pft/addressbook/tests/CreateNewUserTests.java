83089550c287a067050e8f8b958d71444f0b28fe
eelina1205 <eelina1205@gmail.com>
2018-06-27 15:21:12 +0200

    2.8 Создаем общий базоваый класс для классов-помошников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateNewUserTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/CreateNewUserTests.java
@@ -1,16 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.tests.TestBase;
 
 public class CreateNewUserTests extends TestBase {
 
 
     @Test
     public void testCreateNewUser() {
-        app.fillForm();
-        app.submitNewUser();
-        app.goToHomePage();
+        app.getGroupHelper().fillForm();
+        app.getGroupHelper().submitNewUser();
+        app.getGroupHelper().goToHomePage();
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,17 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.tests.TestBase;
 
 public class GroupDeletionTests extends TestBase {
 
 
     @Test
     public void testGroupDeletion() {
-       app.goToGroupPage();
-       app.selectGroup();
-       app.deleteSelectedGroup();
-       app.returnToGroupPage();
+       app.getNavigationHelper().goToGroupPage();
+       app.getGroupHelper().selectGroup();
+       app.getGroupHelper().deleteSelectedGroup();
+       app.getGroupHelper().returnToGroupPage();
 
     }
 
